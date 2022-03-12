1b817f5246e31209b34d7a929fcc9f4df0040c19
krevedko <bespalvovan42@gmail.com>
2022-03-12 08:58:38 +0300

    [CS][Task3] Added charts

--- Computer_security/Task3/task_3.py
+++ Computer_security/Task3/task_3.py
@@ -42,16 +42,22 @@ def generate_float(u0: int, m: int, p: int, n: int):
 
 
 def compute_aperiodic_len(u0: int, m: int, p: int):
-  res = set()
+  elem_dict = dict()
   aperiodic_len = 0
+  current_idx = 0
   u = u0
 
-  while u not in res:
-    aperiodic_len += 1
-    res.add(u)
+  while True:
+    if u not in elem_dict.keys():
+      elem_dict[u] = current_idx
+    else:
+      aperiodic_len = len(elem_dict) - elem_dict[u]
+      break
+
+    current_idx += 1
     u, _ = get_next(u, m, p)
 
-  return aperiodic_len
+  return aperiodic_len, len(elem_dict)
 
 
 def get_expected_val_from_seq(sequence: List[float]):
@@ -96,7 +102,7 @@ def get_points_for_cube(random_array: List[int], q: int = 6, dims: int = 2):
   return points_num_list
 
 
-def compute_xi2(random_array: List[int], q: int = 6, dims: int = 2):
+def compute_xi2(random_array: List[float], q: int = 6, dims: int = 2):
   n = len(random_array) // dims
   m = int(pow(q, dims))
   idx = 0x00
@@ -183,16 +189,23 @@ def plot_2d_graph(arr: List[int], size: int):
   ax.scatter(x_array, y_array, c=arr, cmap="gist_yarg")
 
 
-def plot_exp_val_xi2_dispersion(u_values: List[int], m: int, p: int, length: int):
+def plot_exp_val_xi2_dispersion(u_values: List[int], m: int, p: int, length: int, q: int = 2, dims: int = 3):
   mean_values = []
   dispersion = []
   xi2 = []
+  aperiodic_len = []
+  unique_len = []
 
   for u in u_values:
     res = generate_float(u, m, p, length)
     mean_values.append(get_expected_val_from_seq(res))
     dispersion.append(get_dispersion_from_seq(res))
-    xi2.append(compute_xi2(res))
+    xi2.append(compute_xi2(res, q, dims))
+    aper_len, uni_len = compute_aperiodic_len(u, m, p)
+    aperiodic_len.append(aper_len)
+    unique_len.append(uni_len)
+
+    print(f"{u}, {mean_values[-1]}, {dispersion[-1]}, {xi2[-1]}, {aper_len}, {uni_len}")
 
   print(res)
 
@@ -221,26 +234,88 @@ def plot_exp_val_xi2_dispersion(u_values: List[int], m: int, p: int, length: int
   xi2_fig.scatter(u_values, xi2)
 
 
+def plot_2d_histogram(arr: List[int]):
+  fig = plt.figure(6)
+  ax = fig.add_subplot()
+  # ax.axis([1, len(arr) + 1, 1, len(arr) + 1])
+  # ax.grid(True)
+
+  # ax.set_xticks(np.arange(1, len(arr) + 1, 1))
+  # ax.set_yticks(np.arange(1, max(arr) + 1, max(arr) // 100))
+  # ax.set_ylim(len(arr));
+  ax.set_xlabel('Count')
+  ax.set_ylabel('Point Num')
+  ax.bar(np.arange(1, len(arr) + 1, 1), arr, align='center')
+
+
+def find_min_xi2_disp_mean(u_range: int, m_range: int, p: int, length: int, q:int = 2, dims: int = 3, debug = True):
+  min_xi2 = 1000
+  min_dispersion = 1000
+  min_mean = 1
+
+  min_arr = [None] * 3
+
+  for m in m_range:
+    for u in u_range:
+      int_vect = generate_int(u, m, p, length)
+      float_vect = [num / p for num in int_vect]
+
+      temp_xi2 = compute_xi2(float_vect, q, dims)
+      temp_mean = get_expected_val_from_seq(float_vect)
+      temp_dispersion = get_dispersion_from_seq(float_vect)
+      aper_len, uni_len = compute_aperiodic_len(u, m, p)
+
+      if debug:
+        print(f"{u}, {temp_mean}, {temp_dispersion}, {temp_xi2}, {aper_len}, {uni_len}, {m}")
+
+      if temp_xi2 < min_xi2:
+        min_xi2 = temp_xi2
+        min_arr[0] = (u, m)
+
+      if temp_dispersion < min_dispersion:
+        min_dispersion = temp_dispersion
+        min_arr[1] = (u, m)
+
+      if abs(temp_mean - 0.5) < abs(min_mean - 0.5):
+        min_mean = temp_mean
+        min_arr[2] = (u, m)
+
+      if (uni_len != p - 1):
+        break
+
+
+  print(min_arr)
+
 
 if __name__ == '__main__':
   p = 10007
-  m = 28
-  length = 10000
+  m = 214 # 28
+  length = 15000
+  u0 = 1275
   q = 6
+  dims = 2
 
-  func_list = [compute_xi2, get_expected_val_from_seq, get_dispersion_from_seq]
   # print(get_expected_val(7, 5, 10007, 100000))
 
-  # result_arr = generate_table(range(1, p), m, p, length, func_list[0])
+
+  # find_min_xi2_disp_mean(range(1, p // 2), range(2, p // 2), p, length, q, dims)
 
   plot_exp_val_xi2_dispersion(range(1, p), m, p, length)
 
-  random_vect = generate_float(7, m, p, length)
+  random_vect = generate_float(u0, m, p, length)
 
-  cube_points = get_points_for_cube(random_vect, q)
+  cube_points = get_points_for_cube(random_vect, q, dims)
   plot_3d_graph(cube_points, q)
   plot_2d_graph(cube_points, q)
+  # cube_points = get_points_for_cube(random_vect, q ** dims, 1)
+  plot_2d_histogram(cube_points)
 
+  aper_len, uni_len = compute_aperiodic_len(u0, m, p)
+  print(f"exp_val={get_expected_val_from_seq(random_vect)}, "
+        f"disp={get_dispersion_from_seq(random_vect)}, "
+        f"xi2={compute_xi2(random_vect, q, dims)}, "
+        f"aperiodic_len={aper_len}, "
+        f"unique_len={uni_len}", file=sys.stderr)
   plt.show()
 
   # with PdfPages('plot.pdf') as export_pdf:
@@ -251,5 +326,3 @@ if __name__ == '__main__':
 
   #   export_pdf.savefig()
   #   plt.close()
-
-
