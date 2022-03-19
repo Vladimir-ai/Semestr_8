1752adb6742f445506f8567e391a0b07cfdcf1ef
krevedko <bespalvovan42@gmail.com>
2022-03-19 09:42:07 +0300

    [CS][Task6] Added testing for task_6

--- Computer_security/Task6/task_6.py
+++ Computer_security/Task6/task_6.py
@@ -38,7 +38,7 @@ def array_from_rgb(arr):
   return res
 
 
-def cipher_img(ct: bytearray, path: str, C: int = 2, q: float = 0.01, save_name: str = "Lenna1.png",
+def cipher_img(ct: bytearray, path: str, C: int = 2, q: float = 0.5, save_name: str = "Lenna1.png",
                 random = False, first_num_len_bytes = 2, crc_len_bytes = 2):
   with Image.open(path) as base_img:
     img_array = np.array(base_img) / 255
@@ -58,8 +58,6 @@ def cipher_img(ct: bytearray, path: str, C: int = 2, q: float = 0.01, save_name:
       print(s)
     else:
       s = ct
-      # if len(s) * 8 < n:
-      #   s.extend(bytearray(0) * ((n - len(s) * 8) // 8))
 
 
     if (len(s) * 8 > n):
@@ -76,19 +74,16 @@ def cipher_img(ct: bytearray, path: str, C: int = 2, q: float = 0.01, save_name:
         rgb_cell = cell_from_rgb(img_array[row, col, :])
 
         if curr_bit < first_num_len_bytes * 8:
-          new_val = blue[row, col] + (2 * get_bit_from_num(text_len_bytes, curr_bit) - 1) * q * cell_from_rgb(img_array[row, col, :])
-          blue[row, col] = blue[row, col] + (2 * get_bit_from_num(text_len_bytes, curr_bit) - 1) * q * cell_from_rgb(img_array[row, col, :])
+          new_val = blue[row, col] + (2 * get_bit_from_arr(text_len_bytes.to_bytes(first_num_len_bytes * 8, 'little'), curr_bit) - 1) * q * cell_from_rgb(img_array[row, col, :])
+          blue[row, col] = blue[row, col] + (2 * get_bit_from_arr(text_len_bytes.to_bytes(first_num_len_bytes * 8, 'little'), curr_bit) - 1) * q * cell_from_rgb(img_array[row, col, :])
 
         elif (offset := (curr_bit - first_num_len_bytes * 8)) < len(s) * 8:
           new_val = blue[row, col] + (2 * get_bit_from_arr(s, offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
           blue[row, col] = blue[row, col] + (2 * get_bit_from_arr(s, offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
 
-        elif (offset := (curr_bit - (first_num_len_bytes - len(s)) * 8)) < crc_len_bytes * 8:
-          new_val = blue[row, col] + (2 * get_bit_from_num(crc_value, offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
-          blue[row, col] = blue[row, col] + (2 * get_bit_from_num(crc_value, offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
-
-        else:
-          blue[row, col] = blue[row, col] + (-1) * q * cell_from_rgb(img_array[row, col, :])
+        elif (offset := (curr_bit - (first_num_len_bytes + len(s)) * 8)) < crc_len_bytes * 8:
+          new_val = blue[row, col] + (2 * get_bit_from_arr(crc_value.to_bytes(crc_len_bytes * 8, 'little'), offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
+          blue[row, col] = blue[row, col] + (2 * get_bit_from_arr(crc_value.to_bytes(crc_len_bytes * 8, 'little'), offset) - 1) * q * cell_from_rgb(img_array[row, col, :])
 
         curr_bit += 1
 
@@ -109,6 +104,8 @@ def cipher_img(ct: bytearray, path: str, C: int = 2, q: float = 0.01, save_name:
     img = Image.fromarray(img_copy)
     img.save(save_name)
 
+    return s, dispersion, deviation
+
 
 
 def decipher_img(path: str, C: int = 2, q: float = 2, first_num_len_bytes = 2, crc_len_bytes = 2):
@@ -121,6 +118,10 @@ def decipher_img(path: str, C: int = 2, q: float = 2, first_num_len_bytes = 2, c
     curr_bit = 0
     text_len = 0
     crc_expected = 0
+
+    text_len_bytearray = bytearray(first_num_len_bytes)
+    crc_bytearray = bytearray(crc_len_bytes)
+
     for row in range(C + 1, img_array.shape[1] - C, 1):
       for col in range(C + 1, img_array.shape[0] - C, 1):
         vertical = blue[row - C : row + C, col]
@@ -129,25 +130,28 @@ def decipher_img(path: str, C: int = 2, q: float = 2, first_num_len_bytes = 2, c
         curr_blue = blue[row, col]
         b_ = (sum(vertical) + sum(horizontal) - 2 * blue[row, col]) / (4 * C)
         res = int(blue[row, col] > b_)
+
         if curr_bit < first_num_len_bytes * 8:
-          text_len |= res << curr_bit
+          text_len_bytearray[curr_bit // 8] |= res << (curr_bit % 8)
 
           if curr_bit + 1 == first_num_len_bytes * 8:
+            text_len = int.from_bytes(text_len_bytearray, 'little')
             s.extend(bytearray(text_len))
 
         elif (offset := (curr_bit - first_num_len_bytes * 8)) < text_len * 8:
           s[offset // 8] |= res << (offset % 8)
 
-        elif (offset := (curr_bit - (first_num_len_bytes - text_len) * 8)) < crc_len_bytes * 8:
-          crc_expected |= res << offset
+        elif (offset := (curr_bit - (first_num_len_bytes + text_len) * 8)) < crc_len_bytes * 8:
+          crc_bytearray[offset // 8] |= res << (offset % 8)
 
         else:
           break
 
         curr_bit += 1
 
-    if crc_expected != compute_crc(text_len.to_bytes(first_num_len_bytes * 8, 'little') + s):
-      print("ERROR: CRC mismatch")
+    crc_expected = int.from_bytes(crc_bytearray, 'little')
+    if crc_expected != (res_crc := compute_crc(text_len.to_bytes(first_num_len_bytes * 8, 'little') + s)):
+      print(f"ERROR: CRC mismatch. Actual: {res_crc}, expected: {crc_expected}")
     return s
 
 
@@ -156,29 +160,45 @@ def test(text_len_arr: range, img_path: str):
   shutil.rmtree('test_dir')
   os.mkdir('test_dir')
   plot_arr = []
+  dispersion = []
+  deviation = []
 
   for text_len in text_len_arr:
     bytes_arr = randbytes(text_len)
-    cipher_img(bytes_arr, img_path, save_name="test_dir/"+img_path[:-3]+str(text_len)+".png")
+    _, disp, dev = cipher_img(bytes_arr, img_path, save_name="test_dir/"+img_path[:-3]+str(text_len)+".png")
     result_arr = decipher_img("test_dir/"+img_path[:-3]+str(text_len)+".png")
     xor_result = bytes(a ^ b for (a, b) in zip(bytes_arr, result_arr[:text_len]))
     error_ct = sum([bin(x).count("1") for x in xor_result])
     plot_arr.append(error_ct)
+    dispersion.append(disp)
+    deviation.append(dev)
 
   fig = plt.figure(2)
   sub_plt = fig.add_subplot(121)
   sub_plt.set_xlabel('Text length')
   sub_plt.set_ylabel('Error count')
-  print(list(text_len_arr))
-  print(plot_arr)
   sub_plt.scatter(list(text_len_arr), plot_arr)
+
+  fig_disp = plt.figure(3)
+  fig_disp = fig_disp.add_subplot(121)
+  fig_disp.set_xlabel('Text length')
+  fig_disp.set_ylabel('dispersion')
+  fig_disp.scatter(list(text_len_arr), dispersion)
+
+  fig_dev = plt.figure(4)
+  fig_dev = fig_dev.add_subplot(121)
+  fig_dev.set_xlabel('Text length')
+  fig_dev.set_ylabel('deviation')
+  fig_dev.scatter(list(text_len_arr), deviation)
 
 
 if __name__ == "__main__":
-  cipher_img("aboba".encode('ascii'), "Lenna.png")
+  generated_s, _,_ = cipher_img("aboba".encode('ascii'), "Lenna.png", random=True, save_name='lenna1.png')
   s = decipher_img("Lenna1.png")
-  print(s)
-  # test(range(10, 12), "Lenna.png")
+  xor_result = bytes(a ^ b for (a, b) in zip(generated_s, s))
+  print(sum([bin(x).count("1") for x in xor_result]))
+
+  test(range(10, 100), "Lenna.png")
 
   plt.show()
 
