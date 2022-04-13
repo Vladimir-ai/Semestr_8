eeb3de4db6bd4a6b4941f511fc43321fef617233
krevedko <bespalvovan42@gmail.com>
2022-04-13 10:21:49 +0300

    [ASMR][Lab3] Added tail recursion

--- SMR/lab3/src/Factors.hs
+++ SMR/lab3/src/Factors.hs
@@ -5,18 +5,27 @@ module Factors
 
 
 getFactorCount :: Int -> Int -> Int
-getFactorCount num divisor
-  | mod num divisor == 0 = 1 + getFactorCount (div num divisor) divisor
-  | otherwise = 0
+getFactorCount num divisor =
+  helper num divisor 0
+  where
+    helper :: Int -> Int -> Int -> Int
+    helper number divisr count
+      | mod number divisr == 0 = helper (div number divisr) divisr (count + 1)
+      | otherwise = count
 
 
 getFactor :: Int -> Int -> [Int]
-getFactor num divisor
-  | num <= 1 = []
-  | factorCount > 0 = replicate factorCount divisor ++ getFactor (div num (divisor ^ factorCount)) (divisor + 1)
-  | num < divisor * divisor = [num]
-  | otherwise = getFactor num (divisor + 1)
-  where factorCount = getFactorCount num divisor
+getFactor num divisor =
+  helper num divisor []
+  where
+    helper :: Int -> Int -> [Int] -> [Int]
+    helper number divisr acc
+      | number <= 1 = acc
+      | factorCount > 0 = helper (div number (divisr ^ factorCount)) (divisr + 1) (acc ++ replicate factorCount divisr)
+      | number < divisr * divisr = acc ++ [number]
+      | otherwise = helper number (divisr + 1) acc
+      where factorCount = getFactorCount number divisr
+
 
 
 findAllFactors :: Int -> [Int]
